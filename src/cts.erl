5f426ea3f2bba7ee581bce2682b30ade06fea653
Alex Wilson <alex@cooperi.net>
2015-03-17 22:53:12 +1000

    CTS decryption implementation

--- src/cts.erl
+++ src/cts.erl
@@ -21,7 +21,7 @@ encrypt(Type, Key, IV0, Plain) ->
         _ ->
             {<<>>, IV0}
     end,
-    Padded = <<Plain/binary, 0:(BlockSize - Remainder)/unit:8>>,
+    Padded = <<Plain/binary, 0:(BlockSize - Ln)/unit:8>>,
     Pn_1 = binary:part(Padded, {(Blocks - 2)*BlockSize, BlockSize}),
     Xn_1 = crypto:exor(Cn_2, Pn_1),
     En_1 = crypto:block_encrypt(Type, Key, <<0:BlockSize/unit:8>>, Xn_1),
@@ -31,5 +31,28 @@ encrypt(Type, Key, IV0, Plain) ->
     Cn_1 = crypto:block_encrypt(Type, Key, <<0:BlockSize/unit:8>>, Dn),
     <<CBCPart/binary, Cn_1/binary, Cn/binary>>.
 
-decrypt(Type, Key, IV, Cipher) ->
-    ok.
+decrypt(Type, Key, IV0, Cipher) ->
+    BlockSize = block_size(Type),
+    WholeBlocks = byte_size(Cipher) div BlockSize,
+    Remainder = byte_size(Cipher) rem BlockSize,
+    Ln = if (Remainder > 0) -> Remainder; true -> BlockSize end,
+    Blocks = if (Remainder > 0) -> WholeBlocks + 1; true -> WholeBlocks end,
+    {CBCPart, Cn_2} = case Blocks of 
+        B when B > 2 ->
+            Initial = crypto:block_decrypt(Type, Key, IV0,
+                binary:part(Cipher, {0, (Blocks - 2)*BlockSize})),
+            {Initial, binary:part(Cipher, {(Blocks - 3)*BlockSize, BlockSize})};
+        _ ->
+            {<<>>, IV0}
+    end,
+    Cn_1 = binary:part(Cipher, {(Blocks - 2)*BlockSize, BlockSize}),
+    Cn = binary:part(Cipher, {(Blocks - 1)*BlockSize, Ln}),
+    Dn = crypto:block_decrypt(Type, Key, <<0:BlockSize/unit:8>>, Cn_1),
+    C = <<Cn/binary, 0:(BlockSize - Ln)/unit:8>>,
+    Xn = crypto:exor(Dn, C),
+    <<Pn:Ln/binary, XnTail/binary>> = Xn,
+    En_1 = <<Cn/binary, XnTail/binary>>,
+    Xn_1 = crypto:block_decrypt(Type, Key, <<0:BlockSize/unit:8>>, En_1),
+    Pn_1 = crypto:exor(Xn_1, Cn_2),
+    <<CBCPart/binary, Pn_1/binary, Pn/binary>>.
+
