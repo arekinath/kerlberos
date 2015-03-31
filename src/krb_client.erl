0804a3d775026834fbb359579422fb4f9b6a7718
Alex Wilson <alex@cooperi.net>
2015-03-31 18:54:00 +1000

    fix up etype

--- src/krb_client.erl
+++ src/krb_client.erl
@@ -262,7 +262,8 @@ post_decode(E = #'KRB-ERROR'{'e-data' = EData}, S) when is_binary(EData) ->
 			{E#'KRB-ERROR'{'e-data' = PaDatas2}, S2};
 		_ -> {E, S}
 	end;
-post_decode(R = #'KDC-REP'{'enc-part' = #'EncryptedData'{etype = EType, cipher = EP}}, S = #state{etype = EType, key = Key}) when is_binary(EP) ->
+post_decode(R = #'KDC-REP'{'enc-part' = #'EncryptedData'{etype = ETypeId, cipher = EP}}, S = #state{etype = EType, key = Key}) when is_binary(EP) ->
+	Etype = krb_crypto:etype_to_atom(ETypeId),
 	case (catch krb_crypto:decrypt(EType, Key, EP, [{usage, 3}])) of
 		{'EXIT', _} -> {R, S};
 		Plain ->
