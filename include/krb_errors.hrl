e8a859420b5adecc557f5c1a6364a79d058d22af
Alex Wilson <alex@uq.edu.au>
2015-06-02 17:02:23 +1000

    open-source licensing

--- include/krb_errors.hrl
+++ include/krb_errors.hrl
@@ -1,10 +1,29 @@
 %% kerlberos
 %%
-%% Copyright (c) 2015, Alex Wilson and the University of Queensland
+%% Copyright 2015 Alex Wilson <alex@uq.edu.au>
+%% The University of Queensland
 %% All rights reserved.
 %%
-%% Distributed subject to the terms of the 2-clause BSD license, see
-%% the LICENSE file in the root of the distribution.
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions
+%% are met:
+%% 1. Redistributions of source code must retain the above copyright
+%%    notice, this list of conditions and the following disclaimer.
+%% 2. Redistributions in binary form must reproduce the above copyright
+%%    notice, this list of conditions and the following disclaimer in the
+%%    documentation and/or other materials provided with the distribution.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+%% IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+%% OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+%% IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+%% NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+%% DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+%% THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%
 
 -define(KDC_ERR_NONE, 0).
 -define(KDC_ERR_NAME_EXP, 1).
