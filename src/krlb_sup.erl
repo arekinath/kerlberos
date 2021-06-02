3c30baa37a6af9c4827abbf58576eae5c44073d8
Alex Wilson <alex@uq.edu.au>
2021-06-02 17:36:53 +1000

    Fix supervisor up

--- src/krlb_sup.erl
+++ src/krlb_sup.erl
@@ -49,8 +49,8 @@ start_link() ->
 %%                  modules => modules()}   % optional
 init([]) ->
     SupFlags = #{strategy => one_for_all,
-        intensity => 0,
-        period => 1},
+        intensity => 5,
+        period => 10},
     ChildSpecs = [],
     {ok, {SupFlags, ChildSpecs}}.
 
